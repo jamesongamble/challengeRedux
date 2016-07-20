e28a633e88470536d4ba36c2b3381d8445b3480e
Jameson Gamble <jg092250@ompsft214.local>
2016-07-20 12:21:58 -0700

    Fixes server.js for deploment

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 /*eslint no-console:0 */
 'use strict';
-require('core-js/fn/object/assign');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
-const open = require('open');
+const express = require('express')
+const app = express();
 
-new WebpackDevServer(webpack(config), config.devServer)
-.listen(config.port, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
-  console.log('Opening your system browser...');
-  open('http://localhost:' + config.port + '/webpack-dev-server/');
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html')
+});
+
+var port = process.env.PORT || 3000;
+  
+app.listen( port, function() {
+  console.log('listening on port ' + port);
 });
